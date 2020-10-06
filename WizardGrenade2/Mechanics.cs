71298dbf49e9751a87b5c985f3b57c5ff749be54
Edpacca <eddie@pace.me.uk>
2020-10-06 13:08:41 +0100

    started new static mechanics class

---
+++ WizardGrenade2/Mechanics.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardGrenade2
+{
+    public class Mechanics
+    {
+        public const float GRAVITY = 9.8f;
+
+        public static Vector2 ApplyGravity(GameTime gameTime, Vector2 velocity, float mass)
+        {
+            Vector2 acceleration = new Vector2(0, GRAVITY * mass);
+            return velocity + acceleration * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
