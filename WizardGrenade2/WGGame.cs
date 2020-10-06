6d9b03b73969318a334b8db56cb3ba654ecbbf1b
Edpacca <eddie@pace.me.uk>
2020-10-06 12:52:34 +0100

    Set up basic display variables, and keyboard/mouse input

--- WizardGrenade2/WGGame.cs
+++ WizardGrenade2/WGGame.cs
@@ -6,23 +6,45 @@ namespace WizardGrenade2
 {
     public class WGGame : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private KeyboardState _currentKeyboardState;
+        private KeyboardState _previousKeyboardState;
+        private MouseState _currentMouseState;
+        private MouseState _previousMouseState;
+
+        private Matrix _scale;
+        private float _mainScaleX;
+        private float _mainScaleY;
+
+        private const float TARGET_SCREEN_WIDTH = 1200;
+        private const float TARGET_SCREEN_HEIGHT = TARGET_SCREEN_WIDTH * 0.5625f;
+        private const int SCREEN_RESOLUTION_WIDTH = 1920;
+        private const int SCREEN_RESOLUTION_HEIGHT = 1080;
 
         public WGGame()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            _graphics.PreferredBackBufferWidth = SCREEN_RESOLUTION_WIDTH;
+            _graphics.PreferredBackBufferHeight = SCREEN_RESOLUTION_HEIGHT;
+            Window.AllowUserResizing = true;
         }
 
         protected override void Initialize()
         {
+            _mainScaleX = _graphics.PreferredBackBufferWidth / TARGET_SCREEN_WIDTH;
+            _mainScaleY = _graphics.PreferredBackBufferHeight / TARGET_SCREEN_HEIGHT;
+            _scale = Matrix.CreateScale(new Vector3(_mainScaleX, _mainScaleY, 1));
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
         protected override void UnloadContent()
@@ -32,15 +54,31 @@ namespace WizardGrenade2
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            _currentKeyboardState = Keyboard.GetState();
+            _currentMouseState = Mouse.GetState();
+
+            if (_currentKeyboardState.IsKeyDown(Keys.Escape))
                 Exit();
 
             base.Update(gameTime);
+
+            _previousKeyboardState = _currentKeyboardState;
+            _previousMouseState = _currentMouseState;
         }
+
+        public KeyboardState CurrentKeyboardState() => _currentKeyboardState;
+        public KeyboardState PreviousKeyboardState() => _currentKeyboardState;
+        public MouseState CurrentMouseState() => _currentMouseState;
+        public MouseState PreviousMouseState() => _previousMouseState;
+        public static int GetScreenWidth() => (int)TARGET_SCREEN_WIDTH;
+        public static int GetScreenHeight() => (int)TARGET_SCREEN_HEIGHT;
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin(SpriteSortMode.Immediate, null, SamplerState.PointClamp, null, null, null, _scale);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
