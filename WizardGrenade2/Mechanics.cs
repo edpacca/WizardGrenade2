e45a82a05763aa51d4836e398495d187a2293578
Edpacca <eddie@pace.me.uk>
2020-10-25 23:10:31 +0000

    Added additional vector math functions

--- WizardGrenade2/Mechanics.cs
+++ WizardGrenade2/Mechanics.cs
@@ -41,12 +41,26 @@ namespace WizardGrenade2
             return vector1.X * vector2.X + vector1.Y * vector2.Y;
         }
 
+        public static float CrossProduct(Vector2 vector1, Vector2 vector2)
+        {
+            Vector3 vector1z = new Vector3(vector1.X, vector1.Y, 0);
+            Vector3 vector2z = new Vector3(vector2.X, vector2.Y, 0);
+
+            return Vector3.Cross(vector1z, vector2z).Z;
+
+        }
+
         public static float VectorAngle(Vector2 vector1, Vector2 vector2)
         {
             float v1Mag = VectorMagnitude(vector1);
             float v2Mag = VectorMagnitude(vector2);
 
             return (float)Math.Acos(DotProduct(vector1, vector2) / (v1Mag * v2Mag));
+        }
+
+        public static Vector2 VectorBetweenPoints(Vector2 point1, Vector2 point2)
+        {
+            return point1 - point2;
         }
 
         public static Vector2 ReflectionVector(Vector2 incident, Vector2 normal)
