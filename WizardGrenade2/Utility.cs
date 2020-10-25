b9c122334e2d1a9cf2facdc14e5f820db3a0514b
Edpacca <eddie@pace.me.uk>
2020-10-25 23:11:22 +0000

    Added utility class for generic static methods

---
+++ WizardGrenade2/Utility.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardGrenade2
+{
+    public static class Utility
+    {
+        public static float DifferentialGameTimeValue(GameTime gameTime, int rateFactor, int magnitude)
+        {
+            return (float)gameTime.ElapsedGameTime.TotalSeconds * rateFactor * magnitude;
+        }
+
+    }
+}
