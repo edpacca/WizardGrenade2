606fc299d7fca2ccb0cffc373238bf150e314b3b
Edpacca <eddie@pace.me.uk>
2020-11-06 11:44:48 +0000

    New classes for game indicators

---
+++ WizardGrenade2/HealthBar.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+
+namespace WizardGrenade2
+{
+    class HealthBar : Sprite
+    {
+        private string _fileName;
+        private int _startTeamHealth;
+        private int _displayedTeamHealth;
+        private Vector2 _healthBarPosition;
+        private int _framesH = 1;
+        private int _framesV = 6;
+
+        private Dictionary<string, int[]> _animationState = new Dictionary<string, int[]>
+        {
+            ["bar"] = new int[] { 0, 1, 2, 3, 4, 5, 4, 3, 2, 1 }
+        };
+
+        public HealthBar(int teamNumber, int startTeamHealth)
+        {
+            _fileName = "HealthBar";
+            _startTeamHealth = startTeamHealth;
+            _healthBarPosition = new Vector2((WGGame.GetScreenWidth / 2), WGGame.GetScreenHeight - 20 - (15 * teamNumber));
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            LoadContent(contentManager, _fileName, _framesH, _framesV);
+            _healthBarPosition -= GetSpriteOrigin();
+            LoadAnimationContent(_animationState);
+        }
+
+        public void Update(GameTime gameTime, int actualTeamHealth)
+        {
+            UpdateAnimationState("bar", 1, gameTime);
+            SmoothUpdateHealthBar(gameTime, actualTeamHealth);
+            float healthPercentage = _displayedTeamHealth / _startTeamHealth;
+            SpriteRectangleMaskX(healthPercentage);
+
+        }
+
+        private void SmoothUpdateHealthBar(GameTime gameTime, int actualTeamHealth)
+        {
+            if (_displayedTeamHealth > actualTeamHealth)
+            {
+                _displayedTeamHealth -= (int)(gameTime.ElapsedGameTime.TotalSeconds * 100);
+            }
+            else
+            {
+                _displayedTeamHealth = actualTeamHealth;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            DrawSprite(spriteBatch, _healthBarPosition);
+        }
+    }
+}
---
+++ WizardGrenade2/Marker.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardGrenade2
+{
+    class Marker : Sprite
+    {
+        private readonly string _fileName = "Marker";
+        private Vector2 _position = Vector2.Zero;
+        private Vector2 _markerOffset = new Vector2(0, 36);
+        private const int OSCILLATION_RATE = 2;
+        private const int OSCILLATION_AMPLITUDE = 3;
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            LoadContent(contentManager, _fileName);
+            _markerOffset.X = GetSpriteRectangle().Width / 2;
+        }
+
+        public void Update(GameTime gameTime, Vector2 activePlayerPosition)
+        {
+            _position = activePlayerPosition - _markerOffset;
+            _position.Y += ((float)Math.Sin(gameTime.TotalGameTime.TotalSeconds * OSCILLATION_RATE)) * OSCILLATION_AMPLITUDE;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            DrawSprite(spriteBatch, _position);
+        }
+    }
+}
