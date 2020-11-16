48bdcc0109d2dbd24672cf6a6f53962cdcdbc3b3
Ed <edwardjpace@gmail.com>
2020-11-16 22:16:00 +0000

    Menu options working, with clean solution for selection

--- WizardGrenade2/GameSetup.cs
+++ WizardGrenade2/GameSetup.cs
@@ -14,24 +14,18 @@ namespace WizardGrenade2
         private Vector2 _titlePosition = new Vector2(ScreenSettings.CentreScreenWidth, 100f);
         private List<Sprite> _wizards = new List<Sprite>();
 
-        private int _optionIndex = 0;
-        private const int MIN_TEAMS = 2;
-        private const int MAX_TEAMS = 4;
-        private const int MIN_WIZARDS = 1;
-        private const int MAX_WIZARDS = 1;
         private const int HEALTH_INTERVAL = 25;
-        private const int MIN_HEALTH_INTERVALS = 1;
-        private const int MAX_HEALTH_INTERVALS = 10;
-
-        private int[] _optionValues;
+        private int _selectedOption = 0;
+        private int[,] _optionSettings = new int[3, 3]
+        {
+            {2, 2, 4 },
+            {3, 1, 8},
+            {4, 1, 10}
+        };
 
         public GameSetup()
         {
             GameOptions = new GameOptions();
-            _optionValues = new int[GameOptions.Options.Count];
-            _optionValues[0] = MIN_TEAMS;
-            _optionValues[1] = 3;
-            _optionValues[2] = 100;
         }
 
         public void Initialise()
@@ -53,25 +47,24 @@ namespace WizardGrenade2
 
         public void Update(GameTime gameTime)
         {
-            int deltaValue = 0;
+            int optionDelta = 0;
+            int valueDelta = 0;
 
-            //if (InputManager.WasKeyPressed(Keys.Up))
-            //    _optionIndex = ChangeValue(_optionIndex + 1, 0, _optionValues.Length - 1);
-            //else if (InputManager.WasKeyPressed(Keys.Down))
-            //    _optionIndex = ChangeValue(_optionIndex - 1, 0, _optionValues.Length - 1);
-            //else if (InputManager.WasKeyPressed(Keys.Right))
-            //    deltaValue = 1;
-            //else if (InputManager.WasKeyPressed(Keys.Left))
-            //    deltaValue = -1;
+            if (InputManager.WasKeyPressed(Keys.Down))
+                optionDelta = 1;
+            else if (InputManager.WasKeyPressed(Keys.Up))
+                optionDelta = -1;
 
-                //_optionValues[_optionIndex] = ChangeValue(_optionValues[_optionIndex] + deltaValue, 0, 10);
+            _selectedOption = ChangeValue(_selectedOption + optionDelta, 0, _optionSettings.Length);
 
             if (InputManager.WasKeyPressed(Keys.Right))
-                _optionValues[1]++;
+                valueDelta = 1;
             else if (InputManager.WasKeyPressed(Keys.Left))
-                _optionValues[1]--;
+                valueDelta = -1;
 
-            _optionValues[1] = ChangeValue(_optionValues[1], 1, 8);
+            _optionSettings[_selectedOption, 0] = ChangeValue(_optionSettings[_selectedOption, 0] + valueDelta,
+                _optionSettings[_selectedOption, 1],
+                _optionSettings[_selectedOption, 2]);
         }
 
         private int ChangeValue(int nextValue, int minValue, int maxValue)
@@ -85,24 +78,30 @@ namespace WizardGrenade2
 
             for (int i = 0; i < GameOptions.Options.Count; i++)
             {
-                spriteBatch.DrawString(_optionsFont, GameOptions.Options[i], GameOptions.OptionsLayout[i], Color.Yellow);
+                Color selected = i == _selectedOption ? Color.Red : Color.Yellow;
+
+                spriteBatch.DrawString(_optionsFont, GameOptions.Options[i], GameOptions.OptionsLayout[i], selected);
             }
 
             Vector2 wizardPosition = new Vector2(ScreenSettings.TARGET_WIDTH / 2.3f, GameOptions.OptionsLayout[0].Y);
             _wizards[0].SpriteColour = Color.White;
             for (int i = 0; i < 4; i++)
             {
+                _wizards[i].SpriteColour = _optionSettings[0, 0] <= i ? Color.DarkSlateGray : Color.White;
                 _wizards[i].DrawSprite(spriteBatch, wizardPosition);
                 wizardPosition.X += 80;
             }
 
             wizardPosition = new Vector2(ScreenSettings.TARGET_WIDTH / 2.3f, GameOptions.OptionsLayout[1].Y);
             _wizards[0].SpriteColour = Color.Black;
-            for (int i = 0; i < _optionValues[1]; i++)
+            for (int i = 0; i < _optionSettings[1, 0]; i++)
             {
                 _wizards[0].DrawSprite(spriteBatch, wizardPosition);
                 wizardPosition.X += 80;
             }
+
+            Vector2 healthPosition = new Vector2(ScreenSettings.TARGET_WIDTH / 2.3f, GameOptions.OptionsLayout[2].Y);
+            spriteBatch.DrawString(_optionsFont, (_optionSettings[2, 0] * HEALTH_INTERVAL).ToString(), healthPosition, Color.White);
         }
     }
 }
