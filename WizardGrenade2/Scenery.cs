69093f0fcf8dd8438b8e5c462d7c03685a87ae18
Edpacca <eddie@pace.me.uk>
2020-11-21 19:04:26 +0000

    added scenery

---
+++ WizardGrenade2/Scenery.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardGrenade2
+{
+    class Scenery
+    {
+        private SceneryClouds _foregroundClouds;
+        private SceneryClouds _backgroundClouds;
+        private Sprite _mainCloud;
+        private Vector2 _mainCloudPosition;
+
+        private const int NUMBER_FOREGROUND_CLOUDS = 5;
+        private const int FOREGROUND_CLOUD_SPEED = 30;
+        private const int NUMBER_BACKGROUND_CLOUDS = 15;
+        private const int BACKGROUND_CLOUD_SPEED = 15;
+
+        private Vector2 _foregroundMinPosition;
+        private Vector2 _foregroundMaxPosition;
+        private Vector2 _backgroundMinPosition;
+        private Vector2 _backgroundMaxPosition;
+
+        public Scenery()
+        {
+            _foregroundClouds = new SceneryClouds(NUMBER_FOREGROUND_CLOUDS, FOREGROUND_CLOUD_SPEED);
+            _backgroundClouds = new SceneryClouds(NUMBER_BACKGROUND_CLOUDS, BACKGROUND_CLOUD_SPEED);
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+
+            _mainCloud = new Sprite(contentManager, "MainCloud");
+
+            float mainCloudX = ScreenSettings.CentreScreenWidth - (_mainCloud.GetSpriteRectangle().Width / 2);
+            float mainCloudY = ScreenSettings.TARGET_HEIGHT - 100;
+            _mainCloudPosition = new Vector2(mainCloudX, mainCloudY);
+
+            _foregroundMinPosition = new Vector2(-300, ScreenSettings.TARGET_HEIGHT - 150);
+            _foregroundMaxPosition = new Vector2(ScreenSettings.TARGET_WIDTH + 300, ScreenSettings.TARGET_HEIGHT + _mainCloud.GetSpriteRectangle().Height);
+            _backgroundMinPosition = new Vector2(-300, -300);
+            _backgroundMaxPosition = new Vector2(ScreenSettings.TARGET_WIDTH + 300, ScreenSettings.TARGET_HEIGHT);
+
+            _foregroundClouds.SetCloudLimits(_foregroundMinPosition, _foregroundMaxPosition);
+            _backgroundClouds.SetCloudLimits(_backgroundMinPosition, _backgroundMaxPosition);
+            _backgroundClouds.SetCloudScaleRange(25, 125);
+            _foregroundClouds.LoadContent(contentManager);
+            _backgroundClouds.LoadContent(contentManager);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _foregroundClouds.Update(gameTime);
+            _backgroundClouds.Update(gameTime);
+        }
+
+        public void DrawForeground(SpriteBatch spriteBatch)
+        {
+            _mainCloud.DrawSprite(spriteBatch, _mainCloudPosition);
+            _foregroundClouds.Draw(spriteBatch);
+        }
+
+        public void DrawBackground(SpriteBatch spriteBatch)
+        {
+            _backgroundClouds.Draw(spriteBatch);
+        }
+    }
+}
---
+++ WizardGrenade2/SceneryClouds.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace WizardGrenade2
+{
+    class SceneryClouds
+    {
+        private int _numberOfClouds;
+        private int _cloudSpeed;
+        private Random _random = new Random(); 
+
+        private List<Sprite> _clouds = new List<Sprite>();
+        private List<float> _cloudScales = new List<float>();
+        private int _minScalePercentage = 50;
+        private int _maxScalePercentage = 100;
+
+        private List<Vector2> _cloudPositions = new List<Vector2>();
+        private Vector2 _minPosition = Vector2.Zero;
+        private Vector2 _maxPosition = new Vector2(ScreenSettings.TARGET_WIDTH, ScreenSettings.TARGET_WIDTH);
+
+
+        public SceneryClouds(int numberOfClouds, int cloudSpeed)
+        {
+            _numberOfClouds = numberOfClouds;
+            _cloudSpeed = cloudSpeed;
+        }
+
+        public SceneryClouds(int numberOfClouds, int cloudSpeed, Vector2 minPosition, Vector2 maxPosition) : this (numberOfClouds, cloudSpeed)
+        {
+            SetCloudLimits(minPosition, maxPosition);
+        }
+
+        public SceneryClouds(int numberOfClouds, int cloudSpeed, Vector2 minPosition, Vector2 maxPosition, int minScalePercentage, int maxScalePercentage) : this(numberOfClouds, cloudSpeed, minPosition, maxPosition)
+        {
+            SetCloudScaleRange(minScalePercentage, maxScalePercentage);
+        }
+
+        public void SetCloudLimits(Vector2 minPosition, Vector2 maxPosition)
+        {
+            _minPosition = minPosition;
+            _maxPosition = maxPosition;
+        }
+
+        public void SetCloudScaleRange(int minScalePercentage, int maxScalePercantage)
+        {
+            _minScalePercentage = minScalePercentage;
+            _maxScalePercentage = maxScalePercantage;
+
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            for (int i = 0; i < _numberOfClouds; i++)
+            {
+                int randomX = _random.Next((int)_minPosition.X, (int)_maxPosition.X);
+                int randomY = _random.Next((int)_minPosition.Y, (int)_maxPosition.Y);
+
+                _clouds.Add(new Sprite(contentManager, "Cloud"));
+                _cloudPositions.Add(new Vector2(randomX, randomY));
+                float randomScale = _random.Next(_minScalePercentage, _maxScalePercentage);
+                _cloudScales.Add(randomScale / 100);
+                _clouds[i].SpriteScale = _cloudScales[i];
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            float movement = (float)gameTime.ElapsedGameTime.TotalSeconds * _cloudSpeed;
+
+            for (int i = 0; i < _cloudPositions.Count; i++)
+            {
+                _cloudPositions[i] = _cloudPositions[i].X > _maxPosition.X ?
+                    new Vector2(_minPosition.X + movement * _cloudScales[i], _cloudPositions[i].Y) :
+                    new Vector2(_cloudPositions[i].X + movement * _cloudScales[i], _cloudPositions[i].Y);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < _numberOfClouds; i++)
+                _clouds[i].DrawSprite(spriteBatch, _cloudPositions[i]);
+        }
+    }
+}
