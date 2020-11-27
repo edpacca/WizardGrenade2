ae122337f5e1e3639db3d7d3a4c19f1be6005ede
Edpacca <eddie@pace.me.uk>
2020-11-27 14:26:35 +0000

    implemented new options class in GameSetup, reducing GameOptions to a simple container for parameters

--- WizardGrenade2/GameOptions.cs
+++ WizardGrenade2/GameOptions.cs
@@ -10,27 +10,5 @@ namespace WizardGrenade2
         public int NumberOfTeams { get; set; }
         public int TeamSize { get; set; }
         public string MapFile { get; set; }
-        public readonly List<string> options = new List<string> { "Players", "Wizards", "Health" };
-        public readonly List<string> mapNames = new List<string> { "Castle", "Two-Towers", "City", "Clouds", "Arena" };
-        public List<Vector2> OptionsLayout { get; private set; }
-
-        public List<string> Options { get; set; }
-
-        // To be done, make new class
-        public GameOptions(/*List<string> options*/)
-        {
-            Options = options;
-            OptionsLayout = new List<Vector2>();
-            SetUpOptions();
-        }
-
-        private void SetUpOptions()
-        {
-            int numberOfOptions = options.Count;
-            float verticalInterval = ScreenSettings.TARGET_HEIGHT / (numberOfOptions + 2);
-
-            for (int i = 0; i < numberOfOptions; i++)
-                OptionsLayout.Add(new Vector2(ScreenSettings.TARGET_WIDTH / 5, verticalInterval * (i + 2)));
-        }
     }
 }
--- WizardGrenade2/GameSetup.cs
+++ WizardGrenade2/GameSetup.cs
@@ -11,72 +11,54 @@ namespace WizardGrenade2
         public GameOptions GameOptions { get; private set; }
         private SpriteFont _optionsFont;
         private SpriteFont _infoFont;
-        private OptionArrows _arrows;
-
         private Sprite _title;
+        private Sprite _arrow;
         private Vector2 _titlePosition;
-
-        private const float MAP_SCALE = 0.5f;
-        private Vector2 _mapTextPosition;
         private Vector2 _mapPosition;
-        private List<Sprite> _maps;
-        private string _info = "Go back: 'backspace'";
-        private Vector2 _infoTextPosition;
-
-        private List<Sprite> _wizards;
-        Vector2 _wizardPosition;
-
-        private const int HEALTH_INTERVAL = 25;
+        private Vector2 _arrowLPosition;
+        private Vector2 _arrowRPosition;
         private Vector2 _healthTextPosition;
-
-        private bool _isNewOptionSelected;
-        private bool _isOptionsSet;
+        private Vector2 _wizardPosition;
+        private const float MAP_SCALE = 0.5f;
+        private const float ARROW_SCALE = 6f;
+        private const int HEALTH_INTERVAL = 25;
+        private List<Sprite> _maps;
+        private List<Sprite> _wizards;
+        private Options _battleOptions;
+        private Options _mapOptions;
+        private bool _AreBattleOptionsSet;
         private bool _isMapSet;
-
-        private int _selectedOption = 0;
-        private int _selectedMap = 0;
-        private int[,] _optionSettings = new int[3, 3]
+        public readonly List<string> BattleOptionNames = new List<string> { "Players", "Wizards", "Health" };
+        public readonly List<string> MapNames = new List<string> { "Castle", "Two-Towers", "City", "Clouds", "Arena" };
+        private int[,] _battleOptionValues = new int[3, 3]
         {
             {2, 2, 4 },
-            {3, 1, 8},
-            {4, 1, 10}
+            {3, 1, 8 },
+            {4, 1, 10 }
         };
 
         public GameSetup()
         {
             GameOptions = new GameOptions();
-            _arrows = new OptionArrows(true);
             _maps = new List<Sprite>();
             _wizards = new List<Sprite>();
+            _battleOptions = new Options(BattleOptionNames, true, true);
+            _mapOptions = new Options(MapNames, true, false);
             SetMenuPositions();
         }
 
-        public bool isGameSetup()
-        {
-            return _isMapSet;
-        }
+        public bool isGameSetup() => _isMapSet && _AreBattleOptionsSet;
 
         public void SetMenuPositions()
         {
             _titlePosition = new Vector2(ScreenSettings.CentreScreenWidth, ScreenSettings.TARGET_HEIGHT / 6);
-            _mapTextPosition = new Vector2(ScreenSettings.CentreScreenWidth, ScreenSettings.TARGET_HEIGHT * 0.9f);
+            _mapOptions.SetSinglePosition(new Vector2(ScreenSettings.CentreScreenWidth, ScreenSettings.TARGET_HEIGHT * 0.9f));
+            _battleOptions.SetOptionLayout(new Vector2(ScreenSettings.TARGET_WIDTH / 5, _titlePosition.Y + 150), ScreenSettings.TARGET_HEIGHT * 0.8f);
             _mapPosition = ScreenSettings.ScreenCentre * MAP_SCALE + new Vector2(0, ScreenSettings.TARGET_HEIGHT / 10);
-            _wizardPosition = new Vector2(ScreenSettings.TARGET_WIDTH / 2.3f, GameOptions.OptionsLayout[1].Y);
-            _healthTextPosition = new Vector2(ScreenSettings.TARGET_WIDTH / 2.3f, GameOptions.OptionsLayout[2].Y);
-            _infoTextPosition = new Vector2(ScreenSettings.TARGET_WIDTH, 0);
-        }
-
-        private void SetArrowPositions()
-        {
-            Vector2 fontWidth = _isOptionsSet ?
-            _optionsFont.MeasureString(GameOptions.mapNames[_selectedMap]) :
-            _optionsFont.MeasureString(GameOptions.options[_selectedOption]);
-
-            Vector2 optionPosition = _isOptionsSet ? _mapTextPosition - (fontWidth / 2) : GameOptions.OptionsLayout[_selectedOption];
-
-            optionPosition.Y += fontWidth.Y / 2;
-            optionPosition.X -= 25;
-            _arrows.SetPositions(optionPosition, fontWidth.X + 47);
+            _wizardPosition = new Vector2(ScreenSettings.TARGET_WIDTH / 2.3f, _battleOptions.OptionsLayout[0].Y);
+            _healthTextPosition = new Vector2(ScreenSettings.TARGET_WIDTH / 2.3f, _battleOptions.OptionsLayout[2].Y);
+            _arrowLPosition = new Vector2(20, 20);
+            _arrowRPosition = new Vector2(ScreenSettings.TARGET_WIDTH - 20, 20);
         }
 
         public void LoadContent(ContentManager contentManager)
@@ -93,131 +75,115 @@ namespace WizardGrenade2
                 _maps[i].SpriteScale = 0.5f;
             }
 
-            _optionsFont = contentManager.Load<SpriteFont>("OptionFont");
-            _infoFont = contentManager.Load<SpriteFont>("InfoFont");
-            _infoTextPosition.X -= _infoFont.MeasureString(_info).X;
-            _arrows.LoadContent(contentManager);
-            SetArrowPositions();
+            _battleOptions.LoadContent(contentManager);
+            _mapOptions.LoadContent(contentManager);
 
+            _optionsFont = contentManager.Load<SpriteFont>("OptionFont");
+            _infoFont = contentManager.Load<SpriteFont>("InfoFont2");
             _title = new Sprite(contentManager, "Title");
+            _arrow = new Sprite(contentManager, "MelfsAcidArrow");
+            _arrow.SpriteScale = ARROW_SCALE;
+            _arrowRPosition.X -= _arrow.GetSpriteRectangle().Width * ARROW_SCALE;
         }
 
         public void Update(GameTime gameTime)
         {
-            if (_isOptionsSet)
-                UpdateMapMenu();
-            else
-                UpdateOptionsMenu();
-
-            if (_isNewOptionSelected)
+            if (_AreBattleOptionsSet)
             {
-                SetArrowPositions();
-                _isNewOptionSelected = false;
+                _mapOptions.Update(gameTime);
+                UpdateMapMenu();
             }
-            _arrows.Update2(gameTime);
+
+            else
+            {
+                _battleOptions.Update(gameTime);
+                UpdateOptionsMenu();
+            }
         }
 
         private void UpdateOptionsMenu()
         {
-            int optionChange = InputManager.WasKeyPressed(Keys.Down) ? 1 : InputManager.WasKeyPressed(Keys.Up) ? -1 : 0;
-            _selectedOption = Utility.ChangeValueInLimits(_selectedOption + optionChange, 0, _optionSettings.GetLength(0) - 1);
-
-            _isNewOptionSelected = InputManager.WasKeyPressed(Keys.Down) || InputManager.WasKeyPressed(Keys.Up) ? true : false;
-
             int valueChange = InputManager.WasKeyPressed(Keys.Right) ? 1 : InputManager.WasKeyPressed(Keys.Left) ? -1 : 0 ;
 
-            _optionSettings[_selectedOption, 0] = Utility.ChangeValueInLimits(_optionSettings[_selectedOption, 0] + valueChange,
-                _optionSettings[_selectedOption, 1],
-                _optionSettings[_selectedOption, 2]);
+            _battleOptionValues[_battleOptions.SelectedOption, 0] = Utility.ChangeValueInLimits(
+                _battleOptionValues[_battleOptions.SelectedOption, 0] + valueChange,
+                _battleOptionValues[_battleOptions.SelectedOption, 1],
+                _battleOptionValues[_battleOptions.SelectedOption, 2]);
 
             if (InputManager.WasKeyPressed(Keys.Enter))
             {
                 StoreSettings();
-                _isOptionsSet = true;
-                _isNewOptionSelected = true;
+                _AreBattleOptionsSet = true;
             }
         }
 
         private void UpdateMapMenu()
         {
-            _selectedMap =
-                InputManager.WasKeyPressed(Keys.Right) ? Utility.WrapAroundCounter(_selectedMap, _maps.Count) :
-                InputManager.WasKeyPressed(Keys.Left) ? Utility.WrapAroundNegativeCounter(_selectedMap, _maps.Count) : _selectedMap;
-
-            _isNewOptionSelected = InputManager.WasKeyPressed(Keys.Left) || InputManager.WasKeyPressed(Keys.Right) ? true : false;
-
             if (InputManager.WasKeyPressed(Keys.Back))
-            {
-                _isOptionsSet = false;
-                _isNewOptionSelected = true;
-            }
+                _AreBattleOptionsSet = false;
             
             else if (InputManager.WasKeyPressed(Keys.Enter))
             {
-                GameOptions.MapFile = "Map" + _selectedMap;
+                GameOptions.MapFile = "Map" + _mapOptions.SelectedOption;
                 _isMapSet = true;
             }
         }
 
         private void StoreSettings()
         {
-            GameOptions.NumberOfTeams = _optionSettings[0, 0];
-            GameOptions.TeamSize = _optionSettings[1, 0];
-            GameOptions.WizardHealth = _optionSettings[2, 0] * HEALTH_INTERVAL;
+            GameOptions.NumberOfTeams = _battleOptionValues[0, 0];
+            GameOptions.TeamSize = _battleOptionValues[1, 0];
+            GameOptions.WizardHealth = _battleOptionValues[2, 0] * HEALTH_INTERVAL;
         }
 
         public void ResetGame()
         {
             _isMapSet = false;
-            _isOptionsSet = false;
+            _AreBattleOptionsSet = false;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            _arrow.DrawSprite(spriteBatch, _arrowLPosition + _arrow.GetSpriteOrigin(), Mechanics.PI);
+            _arrow.DrawSprite(spriteBatch, _arrowRPosition);
+            spriteBatch.DrawString(_infoFont, "BACKSPACE", new Vector2(_arrowLPosition.X - 7, _arrowLPosition.Y + 40), Colours.LightGrey);
+            spriteBatch.DrawString(_infoFont, "ENTER", new Vector2(_arrowRPosition.X + 5, _arrowRPosition.Y + 40), Colours.LightGrey);
+
             _title.DrawSpriteAtScale(spriteBatch, _titlePosition, 0.4f);
 
-            if (_isOptionsSet)
+            if (_AreBattleOptionsSet)
                 DrawMapOptions(spriteBatch);
             else
                 DrawGameOptions(spriteBatch);
-
-            _arrows.Draw2(spriteBatch);
         }
 
         public void DrawGameOptions(SpriteBatch spriteBatch)
         {
-            for (int i = 0; i < GameOptions.options.Count; i++)
-            {
-                Color selected = i == _selectedOption ? Color.Yellow : Color.DimGray;
-                spriteBatch.DrawString(_optionsFont, GameOptions.options[i], GameOptions.OptionsLayout[i], selected);
-            }
 
-            _wizardPosition.Y = GameOptions.OptionsLayout[0].Y;
+            _battleOptions.DrawOptions(spriteBatch);
+
+            _wizardPosition.Y = _battleOptions.OptionsLayout[0].Y;
             _wizards[0].SpriteColour = Color.White;
 
             for (int i = 0; i < 4; i++)
             {
-                _wizards[i].SpriteColour = _optionSettings[0, 0] <= i ? Colours.GreyedOut : Color.White;
+                _wizards[i].SpriteColour = _battleOptionValues[0, 0] <= i ? Colours.GreyedOut : Color.White;
                 _wizards[i].DrawSprite(spriteBatch, _wizardPosition + new Vector2(i * 80, 0));
             }
 
-            _wizardPosition.Y = GameOptions.OptionsLayout[1].Y;
+            _wizardPosition.Y = _battleOptions.OptionsLayout[1].Y;
             _wizards[0].SpriteColour = Color.Black;
 
-            for (int i = 0; i < _optionSettings[1, 0]; i++)
-            {
+            for (int i = 0; i < _battleOptionValues[1, 0]; i++)
                 _wizards[0].DrawSprite(spriteBatch, _wizardPosition + new Vector2(i * 80, 0));
-            }
 
-            spriteBatch.DrawString(_optionsFont, (_optionSettings[2, 0] * HEALTH_INTERVAL).ToString(), _healthTextPosition, Color.White);
+            spriteBatch.DrawString(_optionsFont, (_battleOptionValues[2, 0] * HEALTH_INTERVAL).ToString(), _healthTextPosition, Color.White);
         }
 
         private void DrawMapOptions(SpriteBatch spriteBatch)
         {
-            Vector2 textLength = _optionsFont.MeasureString(GameOptions.mapNames[_selectedMap]);
-            _maps[_selectedMap].DrawSprite(spriteBatch, _mapPosition);
-            spriteBatch.DrawString(_optionsFont, GameOptions.mapNames[_selectedMap], _mapTextPosition - (textLength / 2), Color.Yellow);
-            spriteBatch.DrawString(_infoFont, _info, _infoTextPosition, Color.Yellow);
+            _mapOptions.DrawOptions(spriteBatch);
+            _maps[_mapOptions.SelectedOption].DrawSprite(spriteBatch, _mapPosition);
         }
     }
 }
