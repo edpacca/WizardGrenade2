8ece5071c291fae58aed43abb03c945ff1b87015
Edpacca <eddie@pace.me.uk>
2020-11-27 14:25:06 +0000

    modified rotation calc method by applying scaling directly to final vector

--- WizardGrenade2/Sprite.cs
+++ WizardGrenade2/Sprite.cs
@@ -70,10 +70,10 @@ namespace WizardGrenade2
 
         private Vector2 CalcRotationOffset(float rotation)
         {
-            float xOffset = -_spriteRectangle.Width  / 2 * (float)Math.Cos(rotation) + (_spriteRectangle.Height  / 2 * (float)Math.Sin(rotation)) * SpriteScale;
-            float yOffset = -_spriteRectangle.Height  / 2 * (float)Math.Cos(rotation) - (_spriteRectangle.Width  / 2 * (float)Math.Sin(rotation)) * SpriteScale;
+            float xOffset = -_spriteRectangle.Width  / 2 * (float)Math.Cos(rotation) + (_spriteRectangle.Height  / 2 * (float)Math.Sin(rotation));
+            float yOffset = -_spriteRectangle.Height  / 2 * (float)Math.Cos(rotation) - (_spriteRectangle.Width  / 2 * (float)Math.Sin(rotation));
 
-            return new Vector2(xOffset, yOffset);
+            return new Vector2(xOffset, yOffset) * SpriteScale;
         }
 
         public void DrawSprite(SpriteBatch spriteBatch, Vector2 position)
