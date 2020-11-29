e8991c31df4cf0315df052fc7a3a22811811a862
Edpacca <eddie@pace.me.uk>
2020-11-29 16:39:21 +0000

    new settings and gamesettings classes for main menu

---
+++ WizardGrenade2/GameSettings.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardGrenade2
+{
+    public class GameSettings
+    {
+        public int MusicVolume { get; set; }
+        private ValueMinMax _musicVolumeValueMinMax;
+        public int AudioVolume { get; set; }
+        private ValueMinMax _audioVolumeValueMinMax;
+        public int Brightness { get; set; }
+        private ValueMinMax _BrightnessValueMinMax;
+
+        public List<ValueMinMax> _gameSettingsValueMinMax { get; private set; }
+
+        public GameSettings(Vector2 firstPosition, float lastYPosition)
+        {
+            _gameSettingsValueMinMax = new List<ValueMinMax>();
+
+            _musicVolumeValueMinMax = new ValueMinMax(5, 0, 10, "Lute");
+            _audioVolumeValueMinMax = new ValueMinMax(5, 0, 10, "Bell");
+            _BrightnessValueMinMax = new ValueMinMax(5, 0, 10, "Fireball");
+            _gameSettingsValueMinMax.Add(_musicVolumeValueMinMax);
+            _gameSettingsValueMinMax.Add(_audioVolumeValueMinMax);
+            _gameSettingsValueMinMax.Add(_BrightnessValueMinMax);
+
+            Vector2 startPosition = firstPosition;
+            float interval = CalculateYInterval(firstPosition, lastYPosition);
+            foreach (var setting in _gameSettingsValueMinMax)
+            {
+                setting.SetPositions(startPosition, new Vector2(startPosition.X + 250, startPosition.Y));
+                startPosition.Y += interval;
+            }
+        }
+
+        public float CalculateYInterval(Vector2 start, float end)
+        {
+            return (end - start.Y) / (_gameSettingsValueMinMax.Count - 1);
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            foreach (var setting in _gameSettingsValueMinMax)
+            {
+                setting.LoadContent(contentManager);
+                setting.SetScale(1.5f);
+            }
+            _gameSettingsValueMinMax[2].SetScale(4f);
+        }
+
+        public void ChangeValue(int index, int difference)
+        {
+            int value = _gameSettingsValueMinMax[index]._valueMinMax[0];
+
+            _gameSettingsValueMinMax[index]._valueMinMax[0] =
+                value + difference <= _gameSettingsValueMinMax[index]._valueMinMax[1] ? _gameSettingsValueMinMax[index]._valueMinMax[1] :
+                value + difference >= _gameSettingsValueMinMax[index]._valueMinMax[2] ? _gameSettingsValueMinMax[index]._valueMinMax[2] :
+                value + difference;
+        }
+
+        public void DrawSettings(SpriteBatch spriteBatch)
+        {
+            foreach (var setting in _gameSettingsValueMinMax)
+                setting.DrawValue(spriteBatch, setting._valueMinMax[0]);
+        }
+    }
+}
---
+++ WizardGrenade2/Settings.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content;
+
+namespace WizardGrenade2
+{
+    class Settings
+    {
+        private Options _settings;
+        public GameSettings _gameSettings { get; private set; }
+        public bool InSettings { get; set; }
+
+        private readonly List<string> _settingNames = new List<string>()
+        {
+            "Music Volume",
+            "Audio Volume",
+            "Brightness"
+        };
+
+        public Settings(Vector2 menuFirstOptionPosition, float menuLastOptionPosition)
+        {
+            _settings = new Options(_settingNames, true, true);
+            _settings.SetOptionLayout(menuFirstOptionPosition, menuLastOptionPosition);
+            Vector2 _gameSettingsFirstPosition = new Vector2(ScreenSettings.CentreScreenWidth + 50, menuFirstOptionPosition.Y);
+            _gameSettings = new GameSettings(_gameSettingsFirstPosition, menuLastOptionPosition);
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            _settings.LoadContent(contentManager);
+            _gameSettings.LoadContent(contentManager);
+        }
+
+        private void UpdateSettings()
+        {
+            if (InputManager.WasKeyPressed(Keys.Back))
+                InSettings = false;
+
+            int valueChange = InputManager.WasKeyPressed(Keys.Right) ? 1 : InputManager.WasKeyPressed(Keys.Left) ? -1 : 0;
+            _gameSettings.ChangeValue(_settings.SelectedOption, valueChange);
+
+            if (InputManager.WasKeyPressed(Keys.Enter))
+            {
+
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _settings.Update(gameTime);
+            UpdateSettings();
+        }
+
+        public int SetMusicVolume(int volume)
+        {
+            _gameSettings.MusicVolume = volume;
+            return _gameSettings.MusicVolume;
+        }
+
+        public int SetAudioVolume(int volume)
+        {
+            _gameSettings.AudioVolume = volume;
+            return _gameSettings.AudioVolume;
+        }
+
+        public int SetBrightness(int brightness)
+        {
+            _gameSettings.Brightness = brightness;
+            return _gameSettings.Brightness;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _settings.DrawOptions(spriteBatch);
+            _gameSettings.DrawSettings(spriteBatch);
+        }
+    }
+}
