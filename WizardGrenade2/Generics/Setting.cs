57a6d6ac634c28990d95066e980278221376c9aa
Edpacca <eddie@pace.me.uk>
2020-12-07 21:49:02 +0000

    new Setting and SpriteMeter class are generics for any setting.

---
+++ WizardGrenade2/Generics/Setting.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WizardGrenade2.Generics
+{
+    public class Setting
+    {
+        public int IntValue { get; private set; }
+        public float Value { get; private set; }
+        public int MinValue { get; private set; }
+        public int MaxValue { get; private set; }
+        private SpriteMeter _spriteMeter;
+
+        public Setting(int initialValue, int minValue, int maxValue)
+        {
+            IntValue = initialValue;
+            MinValue = minValue;
+            MaxValue = maxValue;
+            Value = (float)IntValue / (float)MaxValue;
+        }
+
+        public void SetValue(int value)
+        {
+            IntValue = value >= MaxValue ? MaxValue : value <= MinValue ? MinValue : value;
+            Value = (float)IntValue / (float)MaxValue;
+        }
+
+        public void ChangeValue(int diff)
+        {
+            SetValue(IntValue + diff);
+        }
+
+        public void LoadContent(ContentManager contentManager, string spriteFile)
+        {
+            _spriteMeter = new SpriteMeter(contentManager, spriteFile);
+        }
+
+        public void SetSpriteMeter(float maxWidth, float spriteScale)
+        {
+            _spriteMeter.Interval = maxWidth / (MaxValue - 1);
+            _spriteMeter.SetSpriteScale(spriteScale);
+        }
+
+        public void DrawSetting(SpriteBatch spriteBatch, Vector2 startPosition)
+        {
+            _spriteMeter.Draw(spriteBatch, startPosition, IntValue);
+        }
+    }
+}
---
+++ WizardGrenade2/Generics/SpriteMeter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WizardGrenade2
+{
+    public class SpriteMeter
+    {
+        private Sprite _sprite;
+        public float Interval { get; set; } = 10f;
+
+        public SpriteMeter(ContentManager contentManager, string fileName)
+        {
+            _sprite = new Sprite(contentManager, fileName);
+        }
+
+        public void SetSpriteScale(float scale)
+        {
+            _sprite.SpriteScale = scale;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, int value)
+        {
+            for (int i = 0; i < value; i++)
+                _sprite.DrawSprite(spriteBatch, new Vector2(position.X + (i * Interval), position.Y));
+        }
+    }
+}
