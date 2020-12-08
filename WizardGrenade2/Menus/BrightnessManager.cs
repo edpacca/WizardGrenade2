22334d4cf51898d8652552a16c78f45e38913ad9
Edpacca <eddie@pace.me.uk>
2020-12-08 22:00:53 +0000

    Resorted to sprite overlays at different opacity for brightness control

--- WizardGrenade2/Menus/BrightnessManager.cs
+++ WizardGrenade2/Menus/BrightnessManager.cs
@@ -1,45 +1,49 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace WizardGrenade2
 {
     public class BrightnessManager
     {
-        private Texture2D _brightnessMask;
         private Texture2D _darknessMask;
+        private Sprite _brightnessMask5;
+        private Sprite _brightnessMask10;
+        private Sprite _brightnessMask15;
         private Rectangle _screen;
-        private Color _brightnessColour = new Color(160, 160, 160, 255);
         private Color _darknessColour = new Color(10, 10, 10, 255);
-
         private float _brightness = 0.5f;
 
-        public BrightnessManager(GraphicsDevice graphics)
+        public BrightnessManager(GraphicsDevice graphics, ContentManager contentManager)
         {
             _screen = new Rectangle(0, 0, (int)ScreenSettings.TARGET_WIDTH, (int)ScreenSettings.TARGET_HEIGHT);
-            _brightnessMask = new Texture2D(graphics, 1, 1);
             _darknessMask = new Texture2D(graphics, 1, 1);            
-            _brightnessMask.SetData(new Color[] { _brightnessColour });
             _darknessMask.SetData(new Color[] { _darknessColour });
-            _brightnessMask.GraphicsDevice.BlendState = BlendState.NonPremultiplied;
             _darknessMask.GraphicsDevice.BlendState = BlendState.NonPremultiplied;
+            _brightnessMask5 = new Sprite(contentManager, @"UserInterface/BrightnessMask5");
+            _brightnessMask10 = new Sprite(contentManager, @"UserInterface/BrightnessMask10");
+            _brightnessMask15 = new Sprite(contentManager, @"UserInterface/BrightnessMask15");
         }
 
         public void SetBrightness(float brightness)
         {
             _brightness = brightness;
-            float bright = 1 - brightness;
             float dark = 0.5f - brightness;
-
-            _brightnessColour.A = (byte)(bright * 255);
             _darknessColour.A = (byte)(dark * 255);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+
             if (_brightness > 0.5f)
-                spriteBatch.Draw(_brightnessMask, _screen, _brightnessColour);
+            {
+                if (_brightness < 0.7f)
+                    _brightnessMask5.DrawSprite(spriteBatch, Vector2.Zero);
+                else if (_brightness < 0.85f)
+                    _brightnessMask10.DrawSprite(spriteBatch, Vector2.Zero);
+                else
+                    _brightnessMask15.DrawSprite(spriteBatch, Vector2.Zero);
+            }
             else if (_brightness < 0.5f)
                 spriteBatch.Draw(_darknessMask, _screen, _darknessColour);
         }
