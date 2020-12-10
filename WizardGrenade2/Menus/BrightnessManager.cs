73c8a60abad81ce384b06482ab02ace23a7936cd
Edpacca <eddie@pace.me.uk>
2020-12-10 16:30:17 +0000

    refactored

--- WizardGrenade2/Menus/BrightnessManager.cs
+++ WizardGrenade2/Menus/BrightnessManager.cs
@@ -11,7 +11,7 @@ namespace WizardGrenade2
         private Sprite _brightnessMask10;
         private Sprite _brightnessMask15;
         private Rectangle _screen;
-        private Color _darknessColour = new Color(10, 10, 10, 255);
+        private Color _darknessColour;
         private float _brightness = 0.5f;
 
         public BrightnessManager(GraphicsDevice graphics, ContentManager contentManager)
@@ -20,6 +20,7 @@ namespace WizardGrenade2
             _darknessMask = new Texture2D(graphics, 1, 1);            
             _darknessMask.SetData(new Color[] { _darknessColour });
             _darknessMask.GraphicsDevice.BlendState = BlendState.NonPremultiplied;
+            _darknessColour = Colours.DarknessMask;
             _brightnessMask5 = new Sprite(contentManager, @"UserInterface/BrightnessMask5");
             _brightnessMask10 = new Sprite(contentManager, @"UserInterface/BrightnessMask10");
             _brightnessMask15 = new Sprite(contentManager, @"UserInterface/BrightnessMask15");
@@ -28,25 +29,24 @@ namespace WizardGrenade2
         public void SetBrightness(float brightness)
         {
             _brightness = brightness;
-            float dark = 0.5f - brightness;
-            _darknessColour.A = (byte)(dark * 255);
+            _darknessColour.A = (byte)((0.5f - _brightness) * 255);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
             if (_brightness > 0.5f)
             {
                 if (_brightness < 0.7f)
                     _brightnessMask5.DrawSprite(spriteBatch, Vector2.Zero);
+
                 else if (_brightness < 0.85f)
                     _brightnessMask10.DrawSprite(spriteBatch, Vector2.Zero);
+
                 else
                     _brightnessMask15.DrawSprite(spriteBatch, Vector2.Zero);
             }
             else if (_brightness < 0.5f)
                 spriteBatch.Draw(_darknessMask, _screen, _darknessColour);
         }
-
     }
 }
